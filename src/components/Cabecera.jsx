716af14c7dfc0e3943a8ca79a677b889c8e3db23
Aferpal <fernandezpalomoabel@gmail.com>
2025-11-27 16:40:53 +0100

    Init project

---
+++ src/components/Cabecera.jsx
@@ -0,0 +1,21 @@
+
+
+
+import "../styles/Cabecera.css"
+
+
+function Cabecera(){
+
+    return (
+        <div className="div-cabecera">
+            <img src="https://www.googleapis.com/download/storage/v1/b/pro-fl-intranet-assets/o/642_Fundaci%C3%B3n_CUDECA%2F2023%2Fong-info%2Fcontact-data%2FCudeca%20logo%20web%20300_1701253893359.jpg?generation=1701253893445244&alt=media"></img>
+            <div>
+                <button>Inicio</button>
+                <button>Eventos</button>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Cabecera;
