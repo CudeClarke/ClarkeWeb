a05f4659fb4c77a8ad8d414e13a49d78378657a2
Aferpal <fernandezpalomoabel@gmail.com>
2025-12-02 20:02:43 +0100

    Feature: added /events/ endpoint in the app for testing it with de EvListApp.jsx

---
+++ src/events/eventsMain.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '../index.css'
+import Cabecera from '../components/Cabecera.jsx'
+import EvListApp
+ from '../components/EvListApp.jsx'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Cabecera></Cabecera>
+    <EvListApp></EvListApp>
+  </StrictMode>
+)
