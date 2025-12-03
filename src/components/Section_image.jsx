1d8258df5ca916660ea8eaac81606b8664432dc6
Daniel Fernández Roldán <danirevellodetoro2016@gmail.com>
2025-12-03 11:32:43 +0100

    Corrected section of information in the right of the menu of buying entrances.

--- src/components/Section_image.jsx
+++ src/components/Section_image.jsx
@@ -1,25 +1,21 @@
 import "../styles/Section_image.css"
-import Flor from "../images/Flor.png"
-import Logo from "../images/Logo.png"
 import mapIcon from '/map-icon.svg'
 import calendarIcon from '/calendar-icon.svg'
-import { ClarkeButton, IS_HEADER} from './Button.jsx';
 
-function Section_image({nombre, ubicacion, fecha, url_first, url_back}){
+function Section_image({nombre, ubicacion, fecha, imageUrl}){
 
     return (
         <div className="div-section">
             <div className="div-section_image">
-                <img className="img-first" src={url_first}></img>
-                <img className="img-back" src={url_back}></img>
+                <img className="img-first" src={imageUrl}></img>
             </div>   
             <div className="div-info">
-                <text className="text-nombre"><strong>{nombre}</strong></text>
+                <text className="text-nombre"><strong>{nombre.toUpperCase()}</strong></text>
                 <div className="div-ubic">
                     <img className="img-map" src={mapIcon}></img>
                     <text className="text-ubic"><strong>{ubicacion}</strong></text>
                 </div>
-                <div className="div-fecha">
+                <div className="div-ubic">
                     <img className="img-calendar" src={calendarIcon}></img>
                     <text className="text-fecha"><strong>{fecha}</strong></text>
                 </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Cabecera from './components/Cabecera.jsx'
+import Section_image from './components/Section_image.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Cabecera></Cabecera>
+    <Section_image nombre={"Hola"} ubicacion={"ETSI"} fecha={"7Nov"} url_first={""} url_back={""}></Section_image>
   </StrictMode>
 )
