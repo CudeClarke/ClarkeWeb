f77f847337c432ddc8a356d704d045d7db39f9f7
Aferpal <fernandezpalomoabel@gmail.com>
2025-12-03 12:00:32 +0100

    adding links to header buttons

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -13,8 +13,12 @@ function Cabecera(){
             <img className="img-logo" src={Logo}></img>
             <img className="img-flor" src={Flor}></img>
             <div className="div-button">
-                <ClarkeButton text={"Inicio"} type={IS_HEADER} size={"1em"}></ClarkeButton>
-                <ClarkeButton text={"Eventos"} type={IS_HEADER} size={"1em"}></ClarkeButton>
+                <a href="/">
+                    <ClarkeButton text={"Inicio"} type={IS_HEADER} size={"1em"}></ClarkeButton>
+                </a>
+                <a href="/events/">
+                    <ClarkeButton text={"Eventos"} type={IS_HEADER} size={"1em"}></ClarkeButton>
+                </a>
             </div>
         </div>
         
