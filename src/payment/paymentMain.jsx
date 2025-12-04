a3a8c334289e26fd07763e4d1ee32e60abeb7aef
aleslobatoo <125024764+aleslobatoo@users.noreply.github.com>
2025-12-04 08:55:58 +0100

    Documentation/payment process (#30)
    * transicion efecto
    
    * primera idea de la pasarela de pago hecha
    
    ---------
    
    Co-authored-by: Aferpal <127872626+Aferpal@users.noreply.github.com>

---
+++ src/payment/paymentMain.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '../index.css'
+import Cabecera from '../components/Cabecera.jsx'
+import PaymentPage from '../components/PaymentPage.jsx'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Cabecera></Cabecera>
+    <PaymentPage></PaymentPage>
+  </StrictMode>
+)
