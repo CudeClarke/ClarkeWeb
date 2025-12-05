8a1ac3a72cdeaee56fa5e316137d823340cef073
Aferpal <fernandezpalomoabel@gmail.com>
2025-12-05 16:13:39 +0100

    Feat: Added SubMenuUserData.jsx and .css in ticket_buy folder for this section of the buying ticket proccess.
    Also refactores CampoTexto so that it can receive an action to do when changing the input value, so parents can recover it and use it.
    
    Added basic funcionality of the SubMenu, all input data is saved in a so called state in json format so that it is easy to implement later the sending of the data to the api server.

--- src/generic/components/CampoTexto.jsx
+++ src/generic/components/CampoTexto.jsx
@@ -1,6 +1,6 @@
 import "../styles/CampoTexto.css"
 
-function CampoTexto({nombre, placeholder, obligatorio}){
+function CampoTexto({nombre, placeholder, obligatorio, value, hasValue = false, handleChange = ()=>{}}){
   var text = ''
   if (obligatorio === 'si'){
     text = '*'
@@ -9,7 +9,7 @@ function CampoTexto({nombre, placeholder, obligatorio}){
 
   return <div className="div-campo">
     <label for="campo">{nombre}</label><p>{text}</p><br/>
-    <input id="campo" type="text" style={{width: "20%",height: "25px"}}  placeholder = {placeholder}></input>
+    <input id="campo" type="text" style={{width: "20%",height: "25px"}}  placeholder = {placeholder} value={hasValue ? value : undefined} onChange={handleChange}></input>
     </div>
 
 }
