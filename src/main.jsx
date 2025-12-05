084c4341d00bb824b5abf2e674689f9aeb8a49bf
Aferpal <fernandezpalomoabel@gmail.com>
2025-12-05 13:26:10 +0100

    fixed the styles of the ticket select rows and added a title to continue button to give more info about the status

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Cabecera from './generic/components/Cabecera.jsx'
-
+import TicketBuyDeploy from './ticket_buy/components/TicketBuyDeploy.jsx'
+import SubMenuTicketSelect from './ticket_buy/components/SubMenuTicketSelect.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Cabecera></Cabecera>
+    <TicketBuyDeploy name={"Seleccionar entrada"}>
+      <SubMenuTicketSelect></SubMenuTicketSelect>
+    </TicketBuyDeploy>
   </StrictMode>
 )
--- src/ticket_buy/components/TicketSelectionRow.jsx
+++ src/ticket_buy/components/TicketSelectionRow.jsx
@@ -10,10 +10,12 @@ function TicketSelectionRow({name, descrip, price, currentAmount, setAmount = (n
             <h2>{name.toUpperCase()}</h2>
             <p>{descrip}</p>
         </div>
-        <div className="ticket-selection-price">
-            {price}€
+        <div className="ticket-selection-row-prices">
+            <div className="ticket-selection-price">
+                <p>{price}€</p>
+            </div>
+            <Contador value={currentAmount} modCallBack={setAmount}></Contador>
         </div>
-        <Contador value={currentAmount} modCallBack={setAmount}></Contador>
     </div>)
 }
 
