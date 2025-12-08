0db53ceaf51323f982071cce827c54515262dc55
Aferpal <127872626+Aferpal@users.noreply.github.com>
2025-12-08 17:20:17 +0100

    Feature/field checking (#43)
    * Fixing multiple styles in ticket buy page:
     - Changed botton text from "Continuar" to "Finalizar" on last step
     - Added a maxheight property to app component and a scroll so that it is not too big
     - Added a small padding in User Data Dropdown menu in bottom to have space between buttons and next menu
     - Added a box shadow to main content for clarity
    
    * Feature: Added field checking for user data in ticket buy page.
    
    Also added error codes and messages to report feedback about potential errors.
    
    With that purpose added a new Alert component meant to display this type of info to the user.
    
    Had to partially refactor the ticket buy app, simplifying the responsiblity of the main.jsx and delegating to the app.jsx.
    
    TODO: send data to api
    
    TODO: receive data from api and render the correct event

--- src/ticket_buy/TicketBuyMain.jsx
+++ src/ticket_buy/TicketBuyMain.jsx
@@ -4,34 +4,12 @@ import '../index.css'
 import Cabecera from '../generic/components/Cabecera.jsx'
 import TicketBuyApp from './components/TicketBuyApp.jsx'
 import Footer from '../generic/components/Footer.jsx'
-import Section_image from './components/Section_image.jsx'
-
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Cabecera></Cabecera>
-    <div style={{
-        backgroundImage: "url('https://design.penpot.app/assets/by-file-media-id/8fd8c29f-33f9-8038-8007-2123360d1547')", 
-        width: "80%", 
-        margin: "auto",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        boxShadow: "0px 2px 2px 2px grey"
-    }}>
-
-    
-      <div style={
-        {
-          display: 'flex',
-          alignItems: "center",
-          backdropFilter: "blur(8px)"
-        }
-        }>
-        <TicketBuyApp></TicketBuyApp>
-        <Section_image nombre={"Evento ejemplo"} ubicacion={"Torremolinos"} fecha={"5 de Diciembre"} imageUrl={"https://design.penpot.app/assets/by-file-media-id/8fd8c29f-33f9-8038-8007-2123360d1547"}></Section_image>
-      </div>
-    </div>
+    <TicketBuyApp></TicketBuyApp>
     <Footer></Footer>
   </StrictMode>
 )
--- src/ticket_buy/utils/fieldValidation/field_validation_functions.js
+++ src/ticket_buy/utils/fieldValidation/field_validation_functions.js
@@ -1,7 +1,61 @@
 
+function validateDni(dni){
+
+  var validChars = 'TRWAGMYFPDXBNJZSQVHLCKET';
+  var nifRexp = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKET]$/i;
+  var nieRexp = /^[XYZ][0-9]{7}[TRWAGMYFPDXBNJZSQVHLCKET]$/i;
+  var str = dni.toString().toUpperCase();
+
+  if (!nifRexp.test(str) && !nieRexp.test(str)) return false; //si no cumple el formato a la calle
+
+  var nie = str
+      .replace(/^[X]/, '0')
+      .replace(/^[Y]/, '1')
+      .replace(/^[Z]/, '2');
+
+  var letter = str.substr(-1); //pillamos la última letra
+
+  var charIndex = parseInt(nie.substr(0, 8)) % 23; //calculamos la letra
+
+  if (validChars.charAt(charIndex) === letter) return true; //si coinciden bien
+
+  return false;
+}
+
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
 
 function isValidUser(data){
-    return true;
+    
+    if( !data.consent || data.name.length == 0 || data.surname.length == 0 || data.email.length == 0 || data.dni.length == 0){ //comprobacion basica, estan los cambios
+        return {status: 400, msg: "Algunos campos obligatorios están vacíos, por favor rellenar antes de continuar." };
+    }
+
+    if( !/^[A-Za-zÁ-Úá-ú]+$/.test(data.name) ){
+        return {status: 400, msg: `Formato del nombre incorrecto: ${data.name}` };
+    }
+
+    if( ! (data.surname.split(' ').every((s)=>{return /^[A-Za-zÁ-Úá-ú]+$/.test(s)}))){
+        return {status: 400, msg: `Formato de los apellidos incorrecto: ${data.surname}` };
+    }
+
+    if( !validateEmail(data.email) ){
+        return {status: 400, msg: `Formato del correo incorrecto: ${data.email}` };
+    }
+
+    if( !validateDni(data.dni) ){
+        return {status: 400, msg: `Formato del DNI/NIE incorrecto: ${data.dni}` };
+    }
+
+    return {status: 200, msg: "OK"};
+
+
 }
 
 export {isValidUser}
