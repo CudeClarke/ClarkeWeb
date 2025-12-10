c9360c143978a1d7e5d15c37c351e8891d6fb20a
Aferpal <fernandezpalomoabel@gmail.com>
2025-12-10 12:07:03 +0100

    Feature: fill main page
    Added a picture from the original cudeca page for the main html

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import CudeWrap from './generic/components/CudeWrap'
+import banner from './generic/assets/banner.jpg'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CudeWrap></CudeWrap>
+    <CudeWrap>
+      <img src={banner} style={{width: "100vw"}}></img>
+    </CudeWrap>
   </StrictMode>
 )
