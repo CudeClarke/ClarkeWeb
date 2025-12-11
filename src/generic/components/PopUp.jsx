f1c72269db3bc42501fcae537896c7d89196cd08
Aferpal <127872626+Aferpal@users.noreply.github.com>
2025-12-11 20:54:49 +0100

    Fix pop up rendering (#61)
    Changed to be rendered directly on document body and not inside of its component so that it is over the whole screen
    
    Changed also the import lines on the component

--- src/generic/components/PopUp.jsx
+++ src/generic/components/PopUp.jsx
@@ -1,11 +1,13 @@
 import '../styles/PopUp.css'
+import { createPortal } from 'react-dom';
 
 function PopUp({children}){
-    return <div className="cover">
+    return createPortal(<div className="cover">
         <div className='pop-up'>
             {children}
         </div>
-    </div>
+    </div>,
+    document.body)
 }
 
 export {PopUp};
