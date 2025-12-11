50d2520b7acccca1aecffd4536c03900b3f4ead9
Aferpal <127872626+Aferpal@users.noreply.github.com>
2025-12-11 21:02:34 +0100

    Feature: added accessible mode for ticket select dropdown (#64)
    Changed and added some styles to have more padding and bigger fonts
    
    Removed collaboration query and moved it to a different pop up in case of accessible mode
    
    Also added a prompt to ensure selected tickets on accessible mode

--- src/ticket_buy/components/SubMenuTicketSelect.jsx
+++ src/ticket_buy/components/SubMenuTicketSelect.jsx
@@ -3,8 +3,8 @@ import '../styles/SubMenuTicketSelect.css'
 import TicketSelectionRow from "./TicketSelectionRow";
 import SubTotalPrice from "./SubTotalPrice";
 import CollaborationQuery from "./CollaborationQuery";
-import { ClarkeButton, IS_CONFIRM } from "../../generic/components/Button.jsx";
-
+import { ClarkeButton, IS_CONFIRM, IS_RETURN } from "../../generic/components/Button.jsx";
+import {PopUp} from "../../generic/components/PopUp.jsx"
 
 function selectedTicketsEmpty(list){
     return list.reduce((a,b)=> a+b) == 0;
@@ -18,12 +18,13 @@ function computeTotal(listSelected, ticket_list){
     return Math.round(total*100) / 100;
 }
 
-function SubMenuTicketSelect({ticket_types, handleConfirm}){
+function SubMenuTicketSelect({ticket_types, handleConfirm, isAccessible}){
 
     const [selectedTickets, setSelectedTickets] = useState(Array(ticket_types.length).fill(0))
 
+    const [accesibleStep, setAccessibleStep] = useState(0);
 
-    return (<div className="sub-menu-ticket-select-container">
+    return (<div className={"sub-menu-ticket-select-container " + (isAccessible ? "sub-menu-ticket-select-accesible" : "")}>
         <div className="sub-menu-ticket-select-tickets">
             {
                 ticket_types.map( (ticket, index) => {
@@ -48,12 +49,18 @@ function SubMenuTicketSelect({ticket_types, handleConfirm}){
         </div>
 
         <hr/>
+        {
+            !isAccessible && (
+                <>
+                    <div className="sub-menu-ticket-select-collaborate">
+                        <CollaborationQuery></CollaborationQuery>
+                    </div>
 
-        <div className="sub-menu-ticket-select-collaborate">
-            <CollaborationQuery></CollaborationQuery>
-        </div>
-
-        <hr/>
+                    <hr/>
+                </>
+            )
+        }
+        
 
         <div className="sub-menu-ticket-select-subtotals">
 
@@ -88,10 +95,72 @@ function SubMenuTicketSelect({ticket_types, handleConfirm}){
                     selectedTicketsEmpty(selectedTickets) ? //no hay nada seleccionado?
                     ()=>{} //no hagas nada
                     : //de lo contrario
+                    isAccessible? //es accesible?
+                    ()=>setAccessibleStep(1) //pasar al siguiente paso
+                    : // finalmente
                     ()=>handleConfirm(selectedTickets) //haz lo tuyo
                 }
                 ></ClarkeButton> 
         </div>
+
+        {/*PARTE ACCESIBLE*/}
+
+        {
+            isAccessible && accesibleStep === 1 && (
+                <PopUp>
+                    <div style={{width:"70vw", padding: "1em", display: "flex", alignItems: "center", flexDirection: "column", gap: "1em"}}>
+                        <h1 style={{textDecoration: "underline"}}>¿Quieres colaborar con Cudeca?</h1>
+                        <div style={{width: "90%"}}>
+                            <CollaborationQuery></CollaborationQuery>
+                        </div>
+                        <ClarkeButton text={"No, gracias"} type={IS_RETURN} size={"1.1em"} click_effect={()=>setAccessibleStep(2)}></ClarkeButton>
+                    </div>
+                </PopUp>
+            )
+        }
+
+        {
+            isAccessible && accesibleStep === 2 && (
+                <PopUp>
+                    <div style={{width:"55vw", padding: "1em", display: "flex", alignItems: "center", flexDirection: "column", gap: "2em"}}>
+                        <h1 style={{textDecoration:"underline"}}>AVISO: entradas seleccionadas</h1>
+                        <div style={{display:"flex", width: "90%", justifyContent: "space-between", alignItems: "center"}}>
+                            <div style={{width: "50%", display:"flex", flexDirection:"column", gap:".5em"}}>
+
+                                {
+                                selectedTickets.map((value, index)=>{
+
+                                    if(value === 0) return;
+
+                                    const name = ticket_types[index].name + " x " + value;
+                                    const price = ticket_types[index].price*value;
+                                    return <SubTotalPrice key={name+Math.round(price*100)/100} name={name} total={Math.round(price*100)/100}></SubTotalPrice>
+
+                                })}
+
+                                <hr/>
+                                <SubTotalPrice name={"Total"} total={computeTotal(selectedTickets, ticket_types)}></SubTotalPrice>
+
+                                
+                            </div>
+
+                            <div style={{width: "40%", display: "flex", flexDirection: "column", gap: ".5em"}}>
+                                <ClarkeButton text={"Continuar"} type={IS_CONFIRM} size={"1.1em"}
+                                    click_effect={()=>{
+                                        setAccessibleStep(0);
+                                        handleConfirm(selectedTickets)
+                                    }}    
+                                ></ClarkeButton>
+                                <ClarkeButton text={"Cancelar"} type={IS_RETURN} size={"1em"}
+                                    click_effect={()=>setAccessibleStep(0)}
+                                ></ClarkeButton>
+                            </div>
+                        </div>
+                    </div>
+                </PopUp>
+            )
+        }
+        
         
     </div>)
 }
