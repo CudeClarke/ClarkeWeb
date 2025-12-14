e62ca606edd9e913a9c8ec0cb818f046b92827e7
Icebear1205 <adrianmlp2015@gmail.com>
2025-12-14 03:20:49 +0100

    Created component Entrada

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Cabecera from './components/Cabecera.jsx'
+import { Entrada } from './components/Entrada.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
