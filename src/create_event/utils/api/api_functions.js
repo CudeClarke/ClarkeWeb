f521615a1f3107372c275aa9e1a87b7c61dcbaab
Aferpal <fernandezpalomoabel@gmail.com>
2025-12-14 23:01:21 +0100

    fixed fields in create event api for upload event
    deleting type attribute (who put that there?)
    
    modifying premios to be a list
    
    setting default image url

--- src/create_event/utils/api/api_functions.js
+++ src/create_event/utils/api/api_functions.js
@@ -5,6 +5,16 @@ async function uploadEvent(data){
 
     data.tags = data.tags.split(" ").filter(e=>e);
 
+    delete data.type;
+
+    if(data.premios){
+        data.premios = data.premios.split(" ").filter(e=>e);
+    }
+
+    if(!data.url){
+        data.url = "default.png";
+    }
+
     data.entradas = data.entradas.map(entrada=>{
         return {
             nombre: entrada.nombre,
@@ -15,11 +25,8 @@ async function uploadEvent(data){
     })
 
     if(import.meta.env.VITE_IS_API_LOCAL == "si"){
-        console.log(data);
         return true;
     }
-
-    console.log(JSON.stringify(data))
 
     const URL = import.meta.env.VITE_API_URL ?? "localhost";
     const PORT = import.meta.env.VITE_API_PORT ?? "8080";
