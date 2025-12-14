f4cee09f4ac73393d29789721deccc14c06526c4
Aferpal <127872626+Aferpal@users.noreply.github.com>
2025-12-14 13:27:17 +0100

    Feature: search box (#81)
    Added a search box in event list page to filter events by name, location or tags

--- src/events/components/EvListApp.jsx
+++ src/events/components/EvListApp.jsx
@@ -5,10 +5,12 @@ import { ExpandedEvent } from './ExpandedEvent.jsx'
 import { useEffect, useState } from 'react';
 import {PopUp} from '/src/generic/components/PopUp.jsx'
 import { fetchEvents } from '../utils/api/api_functions.js';
-
+import '../styles/EvListApp.css'
 function EvListApp(){
 
     const [eventList, setEventList] = useState([]);
+
+    const [searchValue, setSearchValue] = useState('');
 
     useEffect(()=>{
         const fn = async ()=>{
@@ -23,12 +25,30 @@ function EvListApp(){
 
     return (
         <>
+            <div className='event-list-filter-section'>
+                <h1>PRÓXIMOS EVENTOS</h1>
+                <div className='event-list-search-box'>
+                    <svg viewBox='0 0 512 512'>
+                        <path fill="#0fab13" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376C296.3 401.1 253.9 416 208 416 93.1 416 0 322.9 0 208S93.1 0 208 0 416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path>
+                    </svg>
+                    <input type="text" placeholder="Búsqueda" onChange={(e)=>{
+                        setSearchValue(e.target.value.toLocaleLowerCase());
+                    }}/>
+                </div> 
+            </div>
+            
 
             <EventoList>
 
                 {
 
-                    eventList.map((evento)=>{
+                    eventList.filter(evento=>{
+                        return(    
+                            evento.name.toLocaleLowerCase().includes(searchValue) || 
+                            evento.location.toLocaleLowerCase().includes(searchValue) ||
+                            (evento.tag_list.length != 0 && evento.tag_list.some((tag)=>tag.toLocaleLowerCase().includes(searchValue)))
+                        )
+                    }).map((evento)=>{
                         return <Evento 
                                     key={evento.evid+evento.entid+evento.name}
                                     title={evento.name} 
